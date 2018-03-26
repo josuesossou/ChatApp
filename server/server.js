cb6a0dcba76ba5d0e9428f457b95701f922e41a9
josuesossou <josuesossou8@gmail.com>
2018-03-26 19:51:14 -0400

    Added Socket.io feature

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
-const express = require('express');
 const path = require('path');
+const http = require('http');
+
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
 
 const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket)=>{
+    console.log('new user');
 
-app.listen(port, ()=>{
+    socket.on('disconnect', ()=>{
+        console.log('Client disconnect from the server')
+    })
+})
+
+server.listen(port, () => {
     console.log(`server up on port ${port}`)
 })
