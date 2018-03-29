0df40c27f764e8694e45f36f2bc61ce53fb47a8a
josuesossou <josuesossou8@gmail.com>
2018-03-29 09:50:46 -0400

    Add a pretty user interface

---
+++ playground/moment.js
@@ -0,0 +1,6 @@
+const moment = require('moment');
+
+
+let date = moment();
+
+console.log(date.format('H:mm a'))
--- public/js/main.js
+++ public/js/main.js
@@ -7,44 +7,61 @@ socket.on('connect', ()=>{
 });
 
 socket.on('newMessage', (message)=>{
+    let formattedDate = moment().format('H:mm a');
     let li = $('<li></li>');
-    li.text(`from ${message.from}: ${message.text}`)
+    li.text(`from ${message.from} ${formattedDate}: ${message.text}`)
     $('#messages').append(li);
 });
 
-$("#messageForm").submit(function(e){
-    e.preventDefault();
-
-    socket.emit("createMessage", {
-       from: "User",
-       text: $("[name=message]").val(),
-    }, function(){})
-})
-
 socket.on('locationMessage', function(location){
+    let formattedDate = moment().format('H:mm a');
     let li = $('<li></li>');
     let a = $('<a target="_blank">User Location</a>');
 
-    li.text(`from ${location.from}: `);
+    li.text(`from ${location.from} ${formattedDate}: `);
     a.attr('href',location.url);
     li.append(a);
 
     $('#messages').append(li);
 })
 
+
+
+$("#messageForm").submit(function(e){
+    e.preventDefault();
+    let inputMessage = $("[name=message]").val();
+
+    if(inputMessage == '') return;
+
+    socket.emit("createMessage", {
+       from: "User",
+       text: inputMessage
+    }, function(){});
+
+    $("[name=message]").val("");
+})
+
 $("#location").click(function(){
+
+    $('#location').attr('disabled', 'disabled')
 
     if (!navigator.geolocation){
         return alert("This browser's old, doesn't support geolocation")
+        $('#location').removeAttr('disabled')
     }
 
     navigator.geolocation.getCurrentPosition(position =>{
         socket.emit('createLocation', {
             lat: position.coords.latitude,
             lng: position.coords.longitude
-        }, function(k){console.log(k)})
+        }, function(k){
+            $('#location').removeAttr('disabled')
+        })
 
-    }, err=>{alert("Unable to get location")})
+    }, err=>{
+        alert("Unable to get location")
+        $('#location').removeAttr('disabled')
+    })
 })
 
 socket.on('disconnect', ()=>{
